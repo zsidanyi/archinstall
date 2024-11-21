9d2851e6ec4537e619ee54dbc103447e960e644b
zsidanyi <zsidanyi@gmail.com>
2024-11-21 08:51:56 +0100

    postinstall for hotplug montior added

--- stages/02_lightwm/postinstall.sh
+++ stages/02_lightwm/postinstall.sh
@@ -3,5 +3,8 @@
 # Source utils
 . ~/dotfiles/scripts/utils.sh
 
+info "Reload for hotplug-monitor service"
+systemctl daemon-reload
+
 info "Enable lightdm Desktop Manager"
 systemctl enable lightdm.service
